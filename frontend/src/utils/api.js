dc60316112280dd0680b99987ba40a320b884947
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-12-20 17:41:58 +0200

    Update api.js
    eslint changes

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,35 +1,39 @@
-import { getData, deleteData, postData, api } from './helpers.js';
+import {
+  getData, deleteData, postData, api
+} from './helpers.js';
 
 // shop requests
-export function getGateways(){
+export function getGateways() {
   return getData(`${api}/Gateways`)
     .then(gateways => ({ gateways }));
 }
 
-export function getGateway(id){
+export function getGateway(id) {
   return getData(`${api}/Gateways/${id}`)
     .then(gateway => ({ gateway }));
 }
 
-export function addGateway(name, ip){
+export function addGateway(name, ip) {
   return postData(`${api}/Gateways/`,
      {
        name: name,
        ip: ip
-     })
+     }
+  )
   .then(newGateway => newGateway);
 }
 
-export function addDevice(vendor, status, gatewayId){
+export function addDevice(vendor, status, gatewayId) {
   return postData(`${api}/Devices/`,
      {
        vendor: vendor,
        status: status,
        gatewayId: gatewayId
-     })
+     }
+  )
   .then(newDevice => newDevice);
 }
 
-export function deleteDevice(deviceId){
+export function deleteDevice(deviceId) {
   return deleteData(`${api}/Devices/${deviceId}`);
 }
