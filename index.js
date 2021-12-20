a6165e65f620db6a42d008c65a8c70754d086e8a
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-12-20 21:43:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-
 import bodyParser from 'body-parser';
 import routeDevice from './routes/routeDevice.js';
 import routeGateway from './routes/routeGateway.js';
